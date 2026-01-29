47fe53233a01f173ea50031ecd8e3f31f2cd533d
manjularkm90-max <manjularkm90@gmail.com>
2026-01-30 00:06:07 +0530

    Add files via upload

---
+++ patt16.c
@@ -0,0 +1,16 @@
+// #include<stdio.h>
+// int main() {
+//     int n;
+//     scanf("%d",&n);
+//     for (int i=1;i<=n;i++) {
+//         char ch='A';
+//         for (int j=1;j<=i;j++) {
+//             //ch++;
+//             printf("%c",ch);
+//             ch++;
+//         }
+        
+//         printf("\n");
+//     }
+// }
+
