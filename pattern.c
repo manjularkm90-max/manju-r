83a95a3d59959c50c5f5dd031a42e1978338f6d6
manjularkm90-max <manjularkm90@gmail.com>
2026-01-30 17:31:40 +0530

    Add files via upload

--- pattern.c
+++ pattern.c
@@ -1,13 +1,55 @@
+// #include<stdio.h>
+// int main(){
+//     int i,j;
+//     scanf("%d%d",&i,&j);{
+//         for(i=1;i<=5;i++){
+//             for(j=1;j<=5;j++){
+//                 printf("*");
+//             }
+//             printf("\n");
+//         }
+// }
+
+// #include<stdio.h>
+// int main(){
+//     int i,j;
+//     scanf("%d%d",&i,&j);
+//     for(i=1;i<=7;i++){
+//         for(j=1;j<=7;j++){
+//             printf("%d",j);
+//         }
+//         printf("\n");
+
+//     }
+// }
+
 #include<stdio.h>
 int main(){
     int i,j;
-    scanf("%d%d",&i,&j);{
-        for(i=1;i<=5;i++){
-            for(j=1;j<=5;j++){
-                printf("*");
-            }
-            printf("\n");
+    scanf("%d%d",&i,&j);
+    for(i=1;i<=5;i++){
+        for(j=1;j<=i;j++){
+            printf("*");
         }
-
+        for(j=1;j<=2*(5-i);j++){
+            printf(" ");
+        }
+        for(j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(i=5;i>=1;i--){
+        for(j=1;j<=i;j++){
+            printf("*");
+        }
+        for(j=1;j<=2*(5-i);j++){
+            printf(" ");
+        }
+        for(j=1;j<=i;j++){+
+            printf("*");
+        }
+        printf("\n");
     }
 }
+
